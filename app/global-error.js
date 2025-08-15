5cded63475b78c3cbcb58a99b97343377771cf5f
angelamurray7099 <angelamurray.7099@gmail.com>
2025-08-14 21:06:09 -0400

    13

--- app/global-error.js
+++ app/global-error.js
@@ -1,5 +1,1 @@
-// app/global-error.js
-// This file is required by Sentry to ensure React rendering errors are reported in the App Router.
-// See: https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#react-render-errors-in-app-router
 
-export { default } from "@sentry/nextjs/dist/jsx/app/global-error";
